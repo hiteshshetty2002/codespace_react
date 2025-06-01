a1c5ae46234a2231fe9da997b63dc2cc4fa21212
Hitesh C <68242046+hiteshshetty2002@users.noreply.github.com>
2025-06-01 08:09:14 +0000

    updated q4 commit

--- src/Module 3/Introduction to ReactJS/Ques_4.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_4.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Error() {
-    return (
-        <div>
-            <h1>Unclosed tag</h1>
-            <p>Self-closing tag example <img src="example.png"></img></p>
-            <ul>
-                <li>List item 1</li>
-                <li>List item 2</li>
-            </ul>
-        </div>
-    );
+function Error({ imageSrc }) {
+  return (
+    <div>
+      <h1>Properly Closed Tags</h1>
+      <p>
+        Self-closing tag example: <img src={imageSrc} alt="Example" />
+      </p>
+      <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+      </ul>
+    </div>
+  );
 }
+
+// ✅ Add PropTypes for validation
+Error.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+};
 
 export default Error;
 
